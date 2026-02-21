cc6ded209fc809bde3f4a3ace8970005fd680563
maashg3l4 <mohammadsohag1256@gmail.com>
2026-02-22 03:11:44 +0600

    My first contribution

---
+++ basic_python.py
@@ -0,0 +1,7 @@
+# My first python program for GitHub contribution
+print("Hello GitHub! I am starting my Python journey.")
+
+# Simple calculation
+a = 10
+b = 20
+print("The sum of a and b is:", a + b)
