20447e974153530abaf547ff687b9a6d6d8c7384
maashg3l4 <mohammadsohag1256@gmail.com>
2026-02-22 21:48:53 +0600

    Task 17: Multi-line strings

--- string_practice.py
+++ string_practice.py
@@ -1,5 +1,11 @@
 text = "  Python is Amazing!  "
 print("Uppercase:", text.upper())
 print("Lowercase:", text.lower())
-print("Trimmed:", text.strip()) # দুই পাশের খালি জায়গা মুছবে
+print("Trimmed:", text.strip()) 
 print("Replace:", text.replace("Amazing", "Powerful"))
+
+about_ml = """Machine Learning is a subset of AI.
+It focuses on teaching computers to learn from data."""
+print(about_ml)
+print("Total characters:", len(about_ml))
+print("Is 'AI' present?", "AI" in about_ml)
