fcdded65de29a0ee867467d665146a024dff50bd
maashg3l4 <mohammadsohag1256@gmail.com>
2026-02-22 21:48:09 +0600

    Task 16: String operations

---
+++ string_practice.py
@@ -0,0 +1,5 @@
+text = "  Python is Amazing!  "
+print("Uppercase:", text.upper())
+print("Lowercase:", text.lower())
+print("Trimmed:", text.strip()) # দুই পাশের খালি জায়গা মুছবে
+print("Replace:", text.replace("Amazing", "Powerful"))
