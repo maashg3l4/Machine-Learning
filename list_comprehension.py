27789554d4be50a1169732aa7bb531cbeb2226b3
maashg3l4 <mohammadsohag1256@gmail.com>
2026-02-23 23:39:58 +0600

    Task 33

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,3 +1,12 @@
 # 1 theke 10 porjonto sonkhar square (borgo) kora
 squares = [x**2 for x in range(1, 11)]
 print("Squares list:", squares)
+
+import random
+options = ["Linear Regression", "Decision Tree", "Neural Networks"]
+print("Target model for today:", random.choice(options))
+
+import random
+data = [10, 20, 30, 40, 50]
+random.shuffle(data)
+print("Shuffled data for training:", data)
