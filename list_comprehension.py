fcd87340c4a446ad8149884042b1fe59146a36fc
maashg3l4 <mohammadsohag1256@gmail.com>
2026-02-23 23:41:37 +0600

    Task 34

--- list_comprehension.py
+++ list_comprehension.py
@@ -10,3 +10,7 @@ import random
 data = [10, 20, 30, 40, 50]
 random.shuffle(data)
 print("Shuffled data for training:", data)
+
+import math
+print("Value of PI:", math.pi)
+print("Square root of 64:", math.sqrt(64))
