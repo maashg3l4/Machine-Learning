6ff71c74b36cb60d5b192abbcc7bbefb9e327add
maashg3l4 <mohammadsohag1256@gmail.com>
2026-02-26 00:10:25 +0600

    Task 114: Added list comprehension for squares

--- basic_python.py
+++ basic_python.py
@@ -6,8 +6,14 @@ a = 10
 b = 20
 print("The sum of a and b is:", a + b)
 
-# basic_python.py - Phase 1
+
 def welcome_msg(name):
     return f"Hello {name}, welcome to Day 3 of Python practice!"
 
 print(welcome_msg("Sohag"))
+
+
+numbers = [5, 10, 15, 20]
+squared = [x**2 for x in numbers]
+print(f"Original: {numbers}")
+print(f"Squared: {squared}")
